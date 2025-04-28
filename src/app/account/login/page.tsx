5eb596c86ee9a3bbd24af17142843e108f7e1840
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-04-28 10:34:14 -0300

    Criado a  pagina estática de login

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -4,26 +4,35 @@ import styles from "./login.module.scss";
 export default async function LoginPage() {
   return (
     <section className={styles.login}>
-      <div className={styles.left}>
-        <div className={styles.mask}>
-          <h1 className="title">Login</h1>
-          <LoginForm />
-        </div>
-      </div>
+      {/* Fundo branco translúcido */}
+      <div className={styles.overlay} />
 
-      <div className={styles.right}>
-        <div className={styles.presentation}>
-          <h2>ETHOS</h2>
-          <p>CURSOS INTEGRADOS</p>
-          <h3>APRESENTA</h3>
-          <div className={styles.cpac}>
-            <span>C</span>
-            <span>P</span>
-            <span>A</span>
-            <span>C</span>
+      <div className={styles.content}>
+        {/* Lado esquerdo: Login */}
+        <div className={styles.left}>
+          <div className={styles.mask}>
+            <h1 className="title">Login</h1>
+            <LoginForm />
           </div>
-          <p>CURSO DE PSICANÁLISE E ANÁLISES CLÍNICA</p>
-          <blockquote>"Velhos universos, novas dimensões"</blockquote>
+        </div>
+
+        {/* Lado direito: Apresentação */}
+        <div className={styles.right}>
+        <div className={styles.presenterImage}></div>
+          <div className={styles.presentation}>
+            <h2>ETHOS</h2>
+            <p>CURSOS INTEGRADOS</p>
+            <h3>APRESENTA</h3>
+            <div className={styles.cpac}>
+              <span>C</span>
+              <span>P</span>
+              <span>A</span>
+              <span>C</span>
+            </div>
+            <p>CURSO DE PSICANÁLISE E ANÁLISES CLÍNICA</p>
+            <blockquote>"Velhos universos, novas dimensões"</blockquote>
+           
+          </div>
         </div>
       </div>
     </section>
--- src/components/account/login/login-form.tsx
+++ src/components/account/login/login-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import styles from "@/app/account/login/login.module.scss";
+import styles from "@/components/account/login/login-form.module.scss";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");

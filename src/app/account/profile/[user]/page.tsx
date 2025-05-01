d96eaa5f9f5ac762804a358d6afed386524b7eb1
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-01 12:39:32 -0300

    try to fix bug v02

--- src/app/account/profile/[user]/page.tsx
+++ src/app/account/profile/[user]/page.tsx
@@ -1,12 +1,16 @@
-export default async function ProfileUserPage({
-  params,
-}: {
-  params: { user: string };
-}) {
+interface PageProps {
+  params: {
+    user: string;
+  };
+}
+
+export default async function ProfileUserPage({ params }: PageProps) {
+  const {user} = params;
+
   return (
     <section>
-      <h1>Usuário: {params.user}</h1>
-      <p>This is the user profile page for {params.user}.</p>
+      <h1>Usuário: {user}</h1>
+      <p>This is the user profile page for {user}.</p>
     </section>
   );
 }

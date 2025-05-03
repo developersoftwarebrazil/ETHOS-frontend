e5bd3e30aa94d5d9e96e4c001c90dba2a53da677
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-02 22:17:21 -0300

    criado a tela de cria usuário

---
+++ src/actions/user-post.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { USER_POST } from "@/functions/api";
+import apiError from "@/functions/api-error";
+import login from "@/functions/api-error";
+
+export default async function userPost(
+  state: { ok: boolean; error: string },
+  formData: FormData
+) {
+  const username = formData.get("username") as string | null;
+  const email = formData.get("email") as string | null;
+  const password = formData.get("password") as string | null;
+
+  try {
+    if (!username || !email || !password)
+      throw new Error("Email ou usuário já cadastrado.");
+    if (password.length < 6)
+      throw new Error("A Senha deve conter no mínimo 6 dígitos");
+    const { url } = USER_POST();
+    const response = await fetch(url, {
+      method: "POST",
+      body: formData,
+    });
+    if (!response.ok) throw new Error("A senha Ou o usário estão inválidos.");
+
+    const { ok } = await login({ ok: true, error: "" }, formData);
+    if (!ok) throw new Error("Erro ao logar.");
+    return { data: null, ok: true, error: "" };
+  } catch (error: unknown) {
+    return apiError(error);
+  }
+}
--- src/components/account/login/login-create-form.tsx
+++ src/components/account/login/login-create-form.tsx
@@ -2,28 +2,29 @@
 
 import styles from "@/components/account/login/login-form.module.scss";
 import Button from "@/components/forms/buttons/button";
-import Login from "@/actions/login";
+
 
 import { useFormState, useFormStatus } from "react-dom";
 import Input from "@/components/forms/inputs/input";
 import ErrorMessage from "@/components/helpers/error-messager";
 import React from "react";
+import userPost from "@/actions/user-post";
 
 function FormButton() {
   const { pending } = useFormStatus();
   return (
     <>
       {pending ? (
-        <Button disabled={pending}>Enviando...</Button>
+        <Button disabled={pending}>Cadastrando...</Button>
       ) : (
-        <Button disabled={pending}>Entrar</Button>
+        <Button disabled={pending}>Cadastrar</Button>
       )}
     </>
   );
 }
 
 export default function LoginCreate() {
-  const [state, action] = useFormState(Login, {
+  const [state, action] = useFormState(userPost, {
     ok: false,
     error: "",
     data: null,

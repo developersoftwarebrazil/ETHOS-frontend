8565593cb129cafd0a690e93511dfb6463653b42
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-06 11:53:12 -0300

    criado uma página para gerenciar o perfil

---
+++ src/app/account/profile/page.tsx
@@ -0,0 +1,10 @@
+import ProfileUserDashboardForm from "@/components/account/profile/profile-form";
+
+export default async function UserProfile() {
+  return (
+    <div>
+      <h1>User page</h1>
+      <ProfileUserDashboardForm />
+    </div>
+  );
+}
---
+++ src/components/account/profile/profile-form.tsx
@@ -0,0 +1,3 @@
+export default function ProfileUserDashboardForm(){
+  return <div>Home</div>
+}

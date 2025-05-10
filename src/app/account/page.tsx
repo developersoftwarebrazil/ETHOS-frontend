4e0388937838ea81d347408236b9ff176be1a682
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-10 08:36:24 -0300

    adicionado bordas no design do account login

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,10 +1,12 @@
 import LoginPresentationForm from "@/components/account/login/presentations/login-presentation-form";
 import Breadcrumbs from "@/components/breadcrumbs/breadcrumbs";
+import Sidebar from "@/components/menus/sidebars/sidebar";
 
 export default async function AccountPage() {
   return (
     <section>
       <Breadcrumbs />
+      <Sidebar />
       <LoginPresentationForm />
     </section>
   );
--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -4,6 +4,7 @@ export default async function HomeAdminPage() {
   return (
     <section>
       <Breadcrumbs />
+      
       <h1>Home Administador </h1>
       <p>Esta é a página de administração</p>
     </section>

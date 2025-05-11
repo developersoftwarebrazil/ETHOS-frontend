6437fb964b672858752dcb6b9ca1581ecbb49221
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-11 10:22:25 -0300

    criado o component form-body

---
+++ src/components/forms/body/form-body.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./form-body.module.scss";
+
+interface FomrBodyProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function FormBody({ title, children }: FomrBodyProps) {
+  return (
+    <section className={styles.formBody}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.content}>{children}</div>
+    </section>
+  );
+}

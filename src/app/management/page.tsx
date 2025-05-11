f2ef3c56f8a9dc2fb83a2ba1c52d565e5afbeb2d
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-11 16:13:20 -0300

    concertado um bug no código" "

--- src/app/management/page.tsx
+++ src/app/management/page.tsx
@@ -1,8 +1,31 @@
-export default async function HomeAdminPage() {
+'use client'
+import SearchBar from "@/components/forms/searchbars/form-searchbar";
+import FormTable from "@/components/forms/tables/form-table";
+import FormBody from "@/components/forms/body/form-body";
+import { useState } from "react";
+
+export default  function HomeAdminPage() {
+  const [search, setSearch] = useState("");
+
+  const data = [
+    { nome: "João", email: "joao@email.com" },
+    { nome: "Maria", email: "maria@email.com" },
+  ];
+
+  const filtered = data.filter((d) =>
+    d.nome.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <section>
-      <h1>Home Administador </h1>
-      <p>Esta é a página de administração</p>
-    </section>
+    <FormBody title="Selecione usuário para editar">
+      <SearchBar onSearch={setSearch} />
+      <FormTable
+        columns={[
+          { key: "nome", label: "Nome" },
+          { key: "email", label: "E-mail" },
+        ]}
+        data={filtered}
+      />
+    </FormBody>
   );
 }

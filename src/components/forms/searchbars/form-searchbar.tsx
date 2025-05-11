f3080b83a8c0c05d5f5f6f97838180b6cb1635aa
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-11 10:33:29 -0300

    criado o componsent form-searchbar

---
+++ src/components/forms/searchbars/form-searchbar.tsx
@@ -0,0 +1,21 @@
+import { Search } from "lucide-react";
+import styles from "./form-searchbar.module.scss";
+
+interface SearchBarProps {
+  placeholder?: string;
+  onSearch: (value: string) => void;
+}
+
+export default function SearchBar({ placeholder = "Buscar...", onSearch }: SearchBarProps) {
+  return (
+    <div className={styles.searchBar}>
+      <Search className={styles.icon} />
+      <input
+        type="text"
+        className={styles.input}
+        placeholder={placeholder}
+        onChange={(e) => onSearch(e.target.value)}
+      />
+    </div>
+  );
+}

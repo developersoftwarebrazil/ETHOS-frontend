f3f9960d890c3ed273ce87a1229167115f245d39
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-13 08:49:15 -0300

    refatoraro o código login- reset-page

--- src/app/account/login/reset/component/login-reset-form.tsx
+++ src/app/account/login/reset/component/login-reset-form.tsx
@@ -1,25 +1,21 @@
 "use client";
+
+import React from "react";
+import { useFormState, useFormStatus } from "react-dom";
+
 import passwordReset from "@/actions/auth/password-reset";
 
 import styles from "@/app/account/login/styles/login-form.module.scss";
-
 import Button from "@/components/forms/buttons/button";
 import Input from "@/components/forms/inputs/input";
 import ErrorMessage from "@/components/helpers/error-messager";
 
-import { useFormState, useFormStatus } from "react-dom";
-import React from "react";
-
 function FormButton() {
   const { pending } = useFormStatus();
   return (
-    <>
-      {pending ? (
-        <Button disabled={pending}>Resetando...</Button>
-      ) : (
-        <Button disabled={pending}>Resetar senha</Button>
-      )}
-    </>
+    <Button disabled={pending}>
+      {pending ? "Resetando..." : "Resetar senha"}
+    </Button>
   );
 }
 
@@ -44,6 +40,9 @@ export default function LoginResetForm({
         <input type="hidden" name="key" value={keyToken} />
 
         <ErrorMessage error={state.error} />
+        {state.ok && (
+          <p className={styles.success}>Senha alterada com sucesso.</p>
+        )}
 
         <FormButton />
       </div>
--- src/app/account/login/reset/page.tsx
+++ src/app/account/login/reset/page.tsx
@@ -1,44 +1,41 @@
 import { Metadata } from "next";
 
 import styles from "@/app/account/login/styles/login.module.scss";
-
-import LoginResetForm from "@/app/account/login/components/reset/login-reset-form";
+import LoginResetForm from "./component/login-reset-form";
 import BackgroundPresentationForm from "@/components/presentations/background-presentation-form";
 
 export const metadata: Metadata = {
   title: "Resetar a sua senha | ETHOS",
-  description: "Reset a senha da sua conta do site ETHOS.",
+  description: "Resete a senha da sua conta no site ETHOS.",
 };
-type SearchParamsReset = {
+
+type Props = {
   searchParams: Promise<{
     key: string;
     login: string;
   }>;
 };
 
-export default async function ResetPage(props: SearchParamsReset) {
-  const searchParams = await props.searchParams;
-  console.log(searchParams);
+export default async function LoginResetPage({ searchParams }: Props) {
+  const resolvedParams = await searchParams;
+
+  const key = resolvedParams.key ?? "";
+  const login = resolvedParams.login ?? "";
+
   return (
     <section className={styles.login}>
-      {/* Fundo branco translúcido */}
-
       <div className={`${styles.content} container-login`}>
-        {/* Lado esquerdo: Login */}
         <div className={styles.left}>
           <div className={styles.mask}>
             <h1 className="title">Recuperar Senha</h1>
-            <LoginResetForm
-              keyToken={searchParams.key}
-              login={searchParams.login}
-            />
+            <LoginResetForm keyToken={key} login={login} />
           </div>
           <div className={styles.overlay} />
         </div>
-        {/* Lado direito: Apresentação */}
         <BackgroundPresentationForm />
-        <div className={styles.overlay}></div> {/* Overlay vem por último! */}
+        <div className={styles.overlay}></div>
       </div>
     </section>
   );
 }
+ 

93fb45a7140da2f5a8cd3e5f71374c898848ef97
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-05-14 23:01:34 -0300

    estilizado e feito um mock de dados

--- src/components/forms/tables/form-table.tsx
+++ src/components/forms/tables/form-table.tsx
@@ -12,23 +12,35 @@ interface FormTableProps {
 
 export default function FormTable({ columns, data }: FormTableProps) {
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          {columns.map((col) => (
-            <th key={col.key}>{col.label}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, i) => (
-          <tr key={i}>
+    <div className={styles.wrapper}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th className={styles.checkboxCol}>
+              <input type="checkbox" className={styles.checkbox}/>
+            </th>
             {columns.map((col) => (
-              <td key={col.key}>{row[col.key]}</td>
+              <th key={col.key} className={styles.headerCell}>
+                {col.label.toUpperCase()}
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((row, i) => (
+            <tr key={i} className={styles.row}>
+              <td className={styles.checkbox}>
+                <input type="checkbox" />
+              </td>
+              {columns.map((col) => (
+                <td key={col.key} className={styles.cell}>
+                  {row[col.key]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
